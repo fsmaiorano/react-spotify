dd635cb90969c6059f534c5439eaf72fd1b223c4
Fábio <fsmaiorano@gmail.com>
2018-07-27 12:44:29 -0300

    ducks playlists

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles.scss";
-import './config/reactotron';
+import "./config/reactotron";
 
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Header from "./components/Header";
 
 import Routes from "./routes";
+import store from "./store";
 
 const App = () => (
   /**
    * BrowserRouter - Quando envolvemos toda a aplicação com o browser router, todas os componentes terão acesso aos parâmetros da URL.
    */
 
-  <BrowserRouter>
-    <main className="wrapper">
-      <div className="container">
-        <Sidebar />
-        <div className="content">
-          <Header />
-          <Routes />
+  <Provider store={store}>
+    <BrowserRouter>
+      <main className="wrapper">
+        <div className="container">
+          <Sidebar />
+          <div className="content">
+            <Header />
+            <Routes />
+          </div>
         </div>
-      </div>
-      <Player />
-    </main>
-  </BrowserRouter>
+        <Player />
+      </main>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
